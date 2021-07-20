b753c879e4d83a50ea150bc0669b9041c4704f69
Duy Dao <jimmydao@gmail.com>
2021-07-20 15:03:25 -0500

    Finished 10 but thinking I may have to move the SUM elsewhere

--- joins-solution.sql
+++ joins-solution.sql
@@ -55,7 +55,19 @@ GROUP BY "products".description;
 
 ## Stretch
 9. How much was the total cost for each order?
-
+SELECT SUM("line_items".quantity * "products".unit_price), "orders".id
+FROM "orders"
+JOIN "line_items" ON "orders".id = "line_items".order_id
+JOIN "products" ON "line_items".product_id = "products".id
+GROUP BY "orders".id;
 
 10. How much has each customer spent in total?
+SELECT SUM(products.unit_price*line_items.quantity) AS total_cost, customers.last_name, customers.first_name
+FROM "customers"
+JOIN addresses ON addresses.customer_id = customers.id
+JOIN orders ON addresses.id = orders.address_id
+JOIN line_items ON orders.id = line_items.order_id
+JOIN products ON products.id = line_items.product_id
+GROUP BY customers.last_name, customers.first_name;
+
 11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
